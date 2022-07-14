7378ba08783ee145375177ac7325579d5d6f2b15
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-07-14 16:19:29 +0530

    added all component in HomeScreen

--- pages/HomeScreen/index.jsx
+++ pages/HomeScreen/index.jsx
@@ -1,8 +1,20 @@
 import { Box } from "@mui/system";
 import React from "react";
+import Header from "../../src/component/Header";
+import SideBar from "../../src/component/SideBar";
+import Videos from "../../src/component/Videos";
+import style from "./style";
 
 const HomeScreen = () => {
-  return <Box>HomeScreen</Box>;
+  return (
+    <Box sx={style.container}>
+      <Header />
+      <Box sx={style.sidebar_video_container}>
+        <SideBar />
+        <Videos />
+      </Box>
+    </Box>
+  );
 };
 
 export default HomeScreen;
---
+++ pages/HomeScreen/style.jsx
@@ -0,0 +1,13 @@
+const style = {
+  container: {
+    // backgroundColor: 'green',
+    height: "100vh",
+  },
+  sidebar_video_container: {
+    // backgroundColor: "red",
+    display: "flex",
+    gap: "10px",
+  },
+};
+
+export default style;
